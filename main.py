743c1c9c6f431dd3da1d0aed5eb1372b995ea880
Borja Albert Gramaje <bgramaje@mb-air-bgramaje.local>
2024-10-20 17:30:58 +0200

    update structure

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from quart import Quart, render_template, websocket, request, jsonify
 from nats.aio.client import Client as NATS
 from dotenv import load_dotenv
+from pymongo import MongoClient
+import gridfs
+
 import os
 import uuid
+import json
 
 load_dotenv()
 
 app = Quart(__name__)
 nc = NATS()
+
+
+# MongoDB connection
+client = MongoClient('mongodb://localhost:27017/')  # Replace with your MongoDB URI
+db = client['subsync']  # Replace with your database name
+fs = gridfs.GridFS(db)
 
 
 async def initialize_nats():
@@ -53,6 +63,36 @@ async def health_check():
     """
     return jsonify({"status": "200", "health": "OK"}), 200
 
+@app.route("/job", methods=["POST"])
+async def create_job():
+    """
+    function to create a job. Should be like your new project in your workspace
+    """
+    try:
+        job_uuid = str(uuid.uuid4())
+        return jsonify({ "status": 200, "job_id": job_uuid }), 200
+    except Exception as e:
+        return jsonify({"error": f"Failed to publish job: {str(e)}"}), 500
+
+@app.route('/job/<job_id>/upload', methods=['POST'])
+async def upload_video(job_id):
+    try:
+        if 'file' not in request.files:
+            return jsonify({"error": "No file provided"}), 400
+
+        file = request.files['file']
+        
+        # Validate file type (optional)
+        if not file.filename.endswith(('.mp4', '.mov', '.avi')):
+            return jsonify({"error": "File type not supported"}), 400
+
+        # Store the video in GridFS
+        video_id = fs.put(file.stream, filename=file.filename)
+
+        return jsonify({"message": "Video uploaded successfully", "job_id": job_id, "video_id": str(video_id)}), 201
+    except Exception as e:
+        return {'error': str(e)}, 500
+    
 
 @app.route("/job", methods=["POST"])
 async def submit_job():
@@ -66,11 +106,10 @@ async def submit_job():
 
     job_uuid = str(uuid.uuid4())
     try:
-        await nc.publish("job.notifications", job_uuid.encode())
+        nats_message = { "status": 200, "job_id": job_uuid }
+        await nc.publish("job.notification", json.dumps(nats_message).encode())
         print(f"Published job UUID: {job_uuid}")
-        return jsonify(
-            {"status": 200, "job": job_uuid, "message": "Job submitted successfully!"}
-        )
+        return jsonify(nats_message), 200
     except Exception as e:
         return jsonify({"error": f"Failed to publish job: {str(e)}"}), 500
 
@@ -79,4 +118,4 @@ if __name__ == "__main__":
     """
     main.py code
     """
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5500)
